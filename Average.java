5498dc8ae45d725907c4c071ca303dd0ac04c7ba
rabailkhan321 <93083966+rabailkhan321@users.noreply.github.com>
2022-02-24 12:20:34 +0500

    Add files via upload
    calculate average marks of student

---
+++ Average.java
@@ -0,0 +1,17 @@
+public class Average{
+	
+	public static void main(String args[]){
+		
+		Student s1=new Student  ();
+	
+		s1.name="Rabail";
+		s1.marks1=98;
+	    s1.marks2=90;
+			
+		
+	double result=s1.calculateAverage();
+		System.out.println(result);
+		
+	
+	}
+}
---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student
+
+{
+	
+	String name;
+	double marks1;
+	double marks2;
+	
+	double calculateAverage(){
+		double sum=marks1+marks2;
+		return sum/2.0;
+		
+}
+}
+
